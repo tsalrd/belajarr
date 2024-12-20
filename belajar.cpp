232645645bb1f2a36ffad73e95b85610e294a5c9
tsalrd <tsalisrd@gmail.com>
2024-12-20 13:43:04 +0700

    update pertama

--- belajar.cpp
+++ belajar.cpp
@@ -4,9 +4,6 @@ int main() {
     char nama[50];
     int usia;
 
-    // Meminta input nama
-    printf("Masukkan nama Anda: ");
-    fgets(nama, sizeof(nama), stdin);
 
     // Menampilkan output
     printf("\nHalo, %s! Anda berusia %d tahun.\n", nama, usia);
